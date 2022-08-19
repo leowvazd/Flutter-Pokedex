473feec6b371d29934bb2f475d72ba50c5333a9c
Leonardo Vaz <87207693+leowvazd@users.noreply.github.com>
2022-08-19 14:56:07 -0300

    update first class
    commit to checkpoint the end of the first class.

---
+++ pokedex/lib/main.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+import 'package:pokedex/common/models/pokemon.dart';
+import 'package:pokedex/common/repositories/pokemon_repository.dart';
+import 'package:pokedex/features/home/container/home_container.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Pokedex',
+      theme: ThemeData(
+        primarySwatch: Colors.red,
+      ),
+      home: HomeContainer(
+        repository: PokemonRepository(),
+      ),
+    );
+  }
+}
